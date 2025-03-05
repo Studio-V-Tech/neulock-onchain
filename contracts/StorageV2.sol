4e41f2b8828ec3511a0b736b9691628630d423c7
Lucas Neves <devel@studiov.tech>
2025-03-05 22:41:37 +0000

    Add NeuStorage V2 contract with entitlement check

---
+++ contracts/StorageV2.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import {INeuStorage} from "./interfaces/INeuStorage.sol";
+import {Neu} from "./Neu.sol";
+import {NeuEntitlement} from "./Entitlement.sol";
+
+contract NeuStorage is
+    Initializable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable,
+    INeuStorage
+{
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    Neu private _neuContract;
+    mapping(address => bytes) private _userdata;
+    NeuEntitlement private _entitlementContract;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        address defaultAdmin,
+        address upgrader,
+        address neuContractAddress,
+        address entitlementContractAddress
+    ) public initializer {
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(UPGRADER_ROLE, upgrader);
+
+        _neuContract = Neu(neuContractAddress);
+        _entitlementContract = NeuEntitlement(entitlementContractAddress);
+    }
+
+    function saveData(uint256 tokenId, bytes memory data) external payable {
+        // Call with tokenId = 0 if entitlement by token other than the NEU
+        require(_entitlementContract.hasEntitlement(msg.sender), "Caller does not have entitlement");
+
+        if (msg.value > 0 && _neuContract.ownerOf(tokenId) == msg.sender) {
+            _neuContract.increaseSponsorPoints{value: msg.value}(tokenId);
+        }
+
+        _userdata[msg.sender] = data;
+    }
+
+    function retrieveData(address owner) external view returns (bytes memory) {
+        return _userdata[owner];
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
+}
+
